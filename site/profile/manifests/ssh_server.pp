f84a7d1d48e2fef50715f96b8fa2f339d86b50c8
ccordero5500 <38764033+ccordero5500@users.noreply.github.com>
2018-04-26 15:43:55 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCs83pLUFyzVjIa06LLEM1VjcWwLL/xq4Nk+MtwcnvjIWJSWcfgNIdW1A09zn/XRv5X69zBzuOUYYaOSFkINyGCfRYsx2KSuhA42rNp0VFBuiiAvu2kp706hjFAbz5/2VOEN4bUQT51Q+widS6E7DSTba+3iJdHnU5DaN6nSqWAaQ4n3Nzk3Av4wrTMNn4TYWO8uzVG7Wg/xORckkD5e1jr66JayUY2Ygl879cbabfQj3iT/xeel9wtm1Uo+SLIp7z1m9MLdLnx3CCNS2NW57NpK59kayvacZQrHNsqOfSeuK3LC5AAX7y+5550BDpv+Dga/jVoSuQYa5jDsc6+eqnx',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCs83pLUFyzVjIa06LLEM1VjcWwLL/xq4Nk+MtwcnvjIWJSWcfgNIdW1A09zn/XRv5X69zBzuOUYYaOSFkINyGCfRYsx2KSuhA42rNp0VFBuiiAvu2kp706hjFAbz5/2VOEN4bUQT51Q+widS6E7DSTba+3iJdHnU5DaN6nSqWAaQ4n3Nzk3Av4wrTMNn4TYWO8uzVG7Wg/xORckkD5e1jr66JayUY2Ygl879cbabfQj3iT/xeel9wtm1Uo+SLIp7z1m9MLdLnx3CCNS2NW57NpK59kayvacZQrHNsqOfSeuK3LC5AAX7y+5550BDpv+Dga/jVoSuQYa5jDsc6+eqnx',
 	}  
 }
